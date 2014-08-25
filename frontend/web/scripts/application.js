1f6846100497e41dddeff92f788565619041574e
Omar Aguirre <omersguchigu@gmail.com>
2014-08-25 00:14:09 -0500

    registering the new components in application & index file

--- frontend/web/scripts/application.js
+++ frontend/web/scripts/application.js
@@ -1,11 +1,13 @@
 define([
 	'backbone.marionette',
 	'communicator',
-	'../views/main',
+  '../views/main-menu',
+	'../views/content',
+  '../views/footer',
 	'../models/empty'
 ],
 
-function (Marionette, Communicator, MainView, EmptyModel) {
+function (Marionette, Communicator, MainMenuView, ContentView, FooterView, EmptyModel) {
   'use strict';
 
   var emptyModel = new EmptyModel();
@@ -13,18 +15,25 @@ function (Marionette, Communicator, MainView, EmptyModel) {
   // Set a custom data in model to show in template
   emptyModel.set('success', 'Congrats Yeoman for Marionette + Drupal is Working');
 
-
-	var mainView = new MainView({model: emptyModel});
+  var mainMenuView = new MainMenuView({model: emptyModel});
+  var contentView = new ContentView({model: emptyModel});
+  var footerView = new FooterView({model: emptyModel});
 
 	var App = new Marionette.Application();
 
 	/* Add application regions here */
-	App.addRegions({});
+	App.addRegions({
+    mainMenuRegion: "#main-menu-region",
+    contentRegion: "#content-region",
+    footerRegion: '#footer-region'
+  });
+
+  App.mainMenuRegion.show(mainMenuView);
+  App.contentRegion.show(contentView);
+  App.footerRegion.show(footerView);
 
 	/* Add initializers here */
-	App.addInitializer(function () {
-		// Using the render result because we don't have region yet to render
-		document.body.innerHTML = mainView.render().el.innerHTML;
+	App.addInitializer(function (options) {
 		Communicator.mediator.trigger('APP:START');
 	});
 
