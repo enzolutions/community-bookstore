60fe06ab4bc3d390dd17263093a5576a732eb52a
enzo - Eduardo Garcia <enzo@anexusit.com>
2014-08-28 14:52:27 -0600

    Updated RequireJS Config to load Backbone Drupal libraries

--- frontend/web/scripts/init.js
+++ frontend/web/scripts/init.js
@@ -6,37 +6,46 @@ require.config({
 
     shim: {
         backbone: {
-            deps: [
-                'underscore',
-                'jquery'
-              ],
-              exports: 'Backbone'
-            },
-            bootstrap: {
-              deps: ['jquery'],
-              exports: 'jquery'
-            }
-          },
-          paths: {
-            jquery: '../vendor/jquery/jquery',
-            backbone: '../vendor/backbone-amd/backbone',
-            underscore: '../vendor/underscore-amd/underscore',
+          deps: [
+            'underscore',
+            'jquery'
+          ],
+          exports: 'Backbone'
+        },
+        bootstrap: {
+          deps: ['jquery'],
+          exports: 'jquery'
+        },
+        'backbone.drupal': {
+          deps: ['backbone']
+        },
+        'backbone.drupal.services': {
+          deps: ['backbone.drupal']
+        },
+        main: {
+              deps: ['backbone.drupal.services']
+        }
+      },
+      paths: {
+        jquery: '../vendor/jquery/jquery',
+        backbone: '../vendor/backbone-amd/backbone',
+        underscore: '../vendor/underscore-amd/underscore',
 
-            /* alias all marionette libs */
-            'backbone.marionette': '../vendor/backbone.marionette/lib/core/amd/backbone.marionette',
-            'backbone.wreqr': '../vendor/backbone.wreqr/lib/amd/backbone.wreqr',
-            'backbone.babysitter': '../vendor/backbone.babysitter/lib/amd/backbone.babysitter',
-            /* backbone.drupal */
-            'backbone.drupal': '../vendor/backbone.drupal/backbone.drupal',
-            'backbone.drupal.services': '../vendor/backbone.drupal/backbone.drupal.services',
+        /* alias all marionette libs */
+        'backbone.marionette': '../vendor/backbone.marionette/lib/core/amd/backbone.marionette',
+        'backbone.wreqr': '../vendor/backbone.wreqr/lib/amd/backbone.wreqr',
+        'backbone.babysitter': '../vendor/backbone.babysitter/lib/amd/backbone.babysitter',
+        /* backbone.drupal */
+        'backbone.drupal': '../vendor/backbone.drupal/backbone.drupal',
+        'backbone.drupal.services': '../vendor/backbone.drupal/backbone.drupal.services',
 
-            /* twig.js */
-            twig: '../vendor/twig.js/twig',
-            /* alias the bootstrap js lib */
-            bootstrap: '../vendor/bootstrap-sass/dist/js/bootstrap',
+        /* twig.js */
+        twig: '../vendor/twig.js/twig',
+        /* alias the bootstrap js lib */
+        bootstrap: '../vendor/bootstrap-sass/dist/js/bootstrap',
 
-            /* Alias text.js for template loading and shortcut the templates dir to tmpl */
-            text: '../vendor/requirejs-text/text',
-            tmpl: '../templates',
-          },
-        });
+        /* Alias text.js for template loading and shortcut the templates dir to tmpl */
+        text: '../vendor/requirejs-text/text',
+        tmpl: '../templates',
+      },
+    });
