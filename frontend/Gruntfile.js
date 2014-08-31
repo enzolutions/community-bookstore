3480566601a2e99f6d2d3b3a9abbb7186abd340a
Omar Aguirre <omersguchigu@gmail.com>
2014-08-31 00:13:19 -0500

    adding load-grunt-tasks replace gruntloadNpmTsks

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -2,6 +2,12 @@
 
 module.exports = function (grunt) {
 
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
     // configurable paths
     var yeomanConfig = {
         app: 'web',
