6c06c7dd6a65bf3968cf2cff3151b589aeac41c6
Omar Aguirre <omersguchigu@gmail.com>
2014-09-22 20:04:00 -0500

    Update files to the latest version of the generator

--- frontend/web/scripts/application.js
+++ frontend/web/scripts/application.js
@@ -1,63 +1,59 @@
 define([
-	'backbone.marionette',
-	'communicator',
-  '../views/main-menu',
-	'../views/content',
-  '../views/footer',
-	'../models/empty'
+  'backbone.marionette',
+  'router',
+  'routes',
+  'communicator',
+  'regionManager',
+  'regions',
 ],
 
-function (Marionette, Communicator, MainMenuView, ContentView, FooterView, EmptyModel) {
+function (Marionette, Router, Routes, Communicator, RegionManager, Regions) {
   'use strict';
 
-  var emptyModel = new EmptyModel();
-
-  // Set a custom data in model to show in template
-  emptyModel.set('success', 'Congrats Yeoman for Marionette + Drupal is Working');
-
-  var mainMenuView = new MainMenuView({model: emptyModel});
-  var contentView = new ContentView({model: emptyModel});
-  var footerView = new FooterView({model: emptyModel});
-
 	var App = new Marionette.Application();
-  var auth_status = false;
 
-// Set Backend API Information
+  // Set Backend API Information
   Backbone.Drupal.restEndpoint = {
-    root: 'http://example.com:80',
-    dataType: '.json'
+    root: 'http://example.com:80' 
   };
 
-
+  
   // Define auth object, set crossDomain if is necessary
-  var Auth = new Backbone.Drupal.Auth({crossDomain: true});
-
+  var Auth = new Backbone.Drupal.Auth({crossDomain: true });
+  
   // Request executed in sync mode
   // If status is token further ajax will use the proper token
-  // Enable the following line when Drupal Auth is working
-  //var auth_status = Auth.login('admin', 'admin');
+  var auth_status = Auth.login('admin', 'admin');
 
   if(auth_status) {
     // Do something if login works
     console.log('Auth Works');
   }
   else {
+    // Do something if login fails
     console.log('Auth Error');
   }
 
-	/* Add application regions here */
-	App.addRegions({
-    mainMenuRegion: "#main-menu-region",
-    contentRegion: "#content-region",
-    footerRegion: '#footer-region'
-  });
-
-  App.mainMenuRegion.show(mainMenuView);
-  App.contentRegion.show(contentView);
-  App.footerRegion.show(footerView);
-
 	/* Add initializers here */
-	App.addInitializer(function (options) {
+	App.addInitializer(function () {
+
+    this._regionManager = new RegionManager();
+
+    Regions.init(this._regionManager);
+
+    /* Add application regions here */
+    /*this._regionManager.addRegion('mainMenuRegion', '#main-menu-region');
+    this._regionManager.addRegion('contentRegion', '#content-region');
+    this._regionManager.addRegion('footerRegion', '#footer-region');
+*/
+    // Initialize Router
+    this._router = new Router({ App: this});
+
+    // Set routes in router
+    Routes.init(this._router);
+    // Start routing system
+    Backbone.history.start();
+
 		Communicator.mediator.trigger('APP:START');
 	});
 
